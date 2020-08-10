5015cf3539014b0e9b17520244155cedbcc91888
SAURABH <50518930+saurabhkemekar@users.noreply.github.com>
2020-08-10 17:51:14 +0530

    Update stereo.py

--- stereo.py
+++ stereo.py
@@ -3,6 +3,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import open3d as o3d
 
+def decompose_essential_matrix(E,K,pts1,pts2):
+    [U, D, V] = np.linalg.svd(E)
+    diag_arr = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 0]])
+    new_E = U @ diag_arr @ V
+    [U, D, V] = np.linalg.svd(new_E)
+    Y = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+    R1 = - U @ Y @ V
+    R2 = - U @ Y.T @ V
+    t = U[:, 2].reshape(3, 1)
+    R_mat = np.array([R1, R1, R2, R2])
+    T_mat = np.array([t, -t, t, -t])
+    P1 = np.zeros((3, 4))
+    P1[:, :3] = np.eye(3)
+    P1 = K @ P1
+    print(R1, "\n", R2)
+    for i in range(4):
+        P2 = np.concatenate((R_mat[i], T_mat[i]), axis=1)
+        P2 = K @ P2
+        world_pts = cv2.triangulatePoints(P1, P2, pts1, pts2)
+        X, Y, Z = world_pts[:3, :] / world_pts[3, :]
+        Z_ = R_mat[i][2, 0] * X + R_mat[i][2, 1] * Y + R_mat[i][2, 2] * Z + T_mat[i][2]
+        print(len(np.where(Z < 0)[0]), len(np.where(Z_ < 0)[0]))
+        if len(np.where(Z < 0)[0]) == 0:
+            R = R_mat[i]
+            t = T_mat[i]
+            break
+    return R,t
+
 def drawlines(img1,img2,lines,pts1,pts2):
     r,c,ch = img1.shape
     for r,pt1,pt2 in zip(lines,pts1,pts2):
@@ -26,18 +54,11 @@ P1 = 8*3*SADWindowSize**2
 P2 = 32*3*SADWindowSize**2
 
 
-# KL = np.array([[7242.753,0,1079.538], #[7242.753 0 1079.538; 0 7242.753 1018.846; 0 0 1]
-#               [ 0,7242.753,1018.846],
-#               [ 0,0,1]])
-#
-# KR = np.array([[7242.753,0,1588.865],#[7242.753 0 1588.865; 0 7242.753 1018.846; 0 0 1]
-#               [ 0,7242.753,1018.846],
-#               [ 0,0,1]])
-KL = np.array([[3997.684,0, 1176.728], #[3997.684 0 1176.728; 0 3997.684 1011.728; 0 0 1]
+KL = np.array([[3997.684,0, 1176.728],
               [ 0,3997.684,1011.728],
               [ 0,0,1]])
 
-KR = np.array([[3997.684,0,1307.839],##[3997.684 0 1307.839; 0 3997.684 1011.728; 0 0 1]
+KR = np.array([[3997.684,0,1307.839],
               [ 0,3997.684,1011.728],
               [ 0,0,1]])
 dist_coeff = None
@@ -66,25 +87,24 @@ E, mask = cv2.findEssentialMat(pts1, pts2, KL, method=cv2.FM_RANSAC, prob=0.99,
                                threshold=0.4, mask=None)
 pts1 = pts1[mask.ravel() == 1]
 pts2 = pts2[mask.ravel() == 1]
-print("intial = ",pts1[:5],pts2[:5])
 
+print("Using own function")
+R,t = decompose_essential_matrix(E,KL,pts1,pts2)
+print("R = {} \n  t = {}".format(R,t))
 points,R,t,mask = cv2.recoverPose(E,pts1,pts2,R = None,t = None,mask = None)
 K_inv = np.linalg.inv(KL)
 F = K_inv.T @ E @ K_inv
-
+print("R = {} \n t = {}".format(R,t))
 lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1,1,2), 2,F)
 lines1 = lines1.reshape(-1,3)
-#img2,_ = drawlines(imgL,imgR,lines1,pts1,pts2)
 
 # Find epilines corresponding to points in left image (first image) and
 # drawing its lines on right image
 lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1,1,2), 1,F)
 lines2 = lines2.reshape(-1,3)
-#img1,_ = drawlines(imgR,imgL,lines2,pts2,pts1)
-print("F = ",F)
 ###################
 R1,R2,P1,P2= cv2.stereoRectify(KL,None,KL,None,(image_size[1],image_size[0]),R,t,flags = cv2.CALIB_ZERO_DISPARITY)[:4]
-
+#print(R1 @ R2.T) # these gives the rotation between the two camera
 mapx1,mapy1 = cv2.initUndistortRectifyMap(KL,None,R1,P1,(image_size[1],image_size[0]),cv2.CV_16SC2)
 mapx2,mapy2 = cv2.initUndistortRectifyMap(KL,None,R2,P2,(image_size[1],image_size[0]),cv2.CV_16SC2)
 print("shape = ",mapx1.shape,mapy1.shape)
@@ -92,18 +112,13 @@ print("shape = ",mapx1.shape,mapy1.shape)
 rectified_imgL = cv2.remap(imgL,mapx1,mapy1,interpolation=cv2.INTER_CUBIC,borderMode=cv2.BORDER_CONSTANT)
 rectified_imgR = cv2.remap(imgR,mapx2,mapy2,interpolation=cv2.INTER_CUBIC,borderMode=cv2.BORDER_CONSTANT)
 
-cv2.imwrite('Rectified0.jpg',rectified_imgL)
-cv2.imwrite('Rectified1.jpg',rectified_imgR)
+
 ###################
 lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1,1,2), 2,F)
 lines1 = lines1.reshape(-1,3)
-#img4,_ = drawlines(rectified_imgL,rectified_imgR,lines1,pts1,pts2)
 
-# Find epilines corresponding to points in left image (first image) and
-# drawing its lines on right image
 lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1,1,2), 1,F)
 lines2 = lines2.reshape(-1,3)
-#img3,_ = drawlines(rectified_imgR,rectified_imgL,lines2,pts2,pts1)
 
 rectified_imgL = cv2.pyrDown(rectified_imgL)
 rectified_imgR = cv2.pyrDown(rectified_imgR)
@@ -114,8 +129,6 @@ left_matcher = cv2.StereoSGBM_create(minDisparity=min_disparity,numDisparities=n
 
 left_disparity = left_matcher.compute(rectified_imgL,rectified_imgR)
 
-cv2.imshow("disparity_left",left_disparity)
-cv2.imwrite("left_disparity.png",left_disparity)
 ##################
 right_matcher = cv2.ximgproc.createRightMatcher(left_matcher)
 right_disparity = right_matcher.compute(rectified_imgR,rectified_imgL)
@@ -135,6 +148,9 @@ filtered_disparity = (filtered_disparity/16).astype(np.uint8)
 cv2.imshow('filter',filtered_disparity)
 cv2.imwrite("wls_disparity.png",filtered_disparity)
 
+depth_map = KL[0,0]*b / (filtered_disparity)
+depth_map = depth_map.astype('uint16')
+cv2.imshow('depth map',depth_map)
 # Reprojection matrix
 Q = np.float32([[1,0,0,-KL[0,2]],
                 [0,1,0,-KL[1,2]],
